aee6e96e88fdafd3da317879ce3eea0e43914367
fraukesch <frauke.schmidt@bettermarks.com>
2024-10-20 23:00:37 +0200

    feat: init fund-o-meter for October

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import {Box, Center, Flex, Heading, Progress, Text} from '@chakra-ui/react'
+
+const donations = {
+    Oct: [196, 66.50, 89.90]
+}
+
+function App() {
+  const oktSum = donations.Oct.reduce((val, acc) => acc + val, 0);
+  const oktPerc = oktSum/4;
+  return (
+      <Flex m={4} flexDirection='column'>
+        <Heading as='h1' size='lg' mb={2}>Lola fund-o-meter</Heading>
+        <Heading as='h2' size='md'>October</Heading>
+        <Center m={4}>
+            <Box position='relative' width='80%'>
+                <Progress
+                    value={oktPerc}
+                    colorScheme='purple'
+                />
+                <Flex width='100%' justifyContent='space-between' mt={2}>
+                    <Text fontWeight='bold' color='purple.600'>{`${oktSum} €`}</Text>
+                    <Text>400 €</Text>
+                </Flex>
+            </Box>
+        </Center>
+      </Flex>
+  )
+}
+
+export default App
