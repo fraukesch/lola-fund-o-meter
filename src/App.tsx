4275cc5b8daeb96f9c5670e385205b50c1cd8567
fraukesch <frauke.schmidt@bettermarks.com>
2024-12-06 18:50:36 +0100

    feat: + remnants of Nov + first two sessions in Dec

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import {Month} from './Month.tsx';
 
 const donations = {
     Oct: [196, 66.50, 89.90, 62.50],
-    Nov: [14.90, 132.60, 111, 62, 74.50, 95, 164, 79.10]
+    Nov: [14.90, 132.60, 111, 62, 74.50, 95, 164, 79.10],
+    Dec: [33.10, 175]
 }
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Heading as='h1' size='lg' mb={2}>Lola Fund-O-Meter</Heading>
         <Month name='October' sum={donations.Oct.reduce((val, acc) => acc + val, 0)} sundays={4} />
         <Month name='November' sum={donations.Nov.reduce((val, acc) => acc + val, 0)} sundays={4} wednesdays={4}/>
+        <Month name='December' sum={donations.Dec.reduce((val, acc) => acc + val, 0)} sundays={4} wednesdays={3}/>
       </Flex>
   )
 }
