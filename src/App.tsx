119a07f70f9e41c81dc5cef281bf02491bd500f0
fraukesch <frauke.schmidt@bettermarks.com>
2025-09-30 22:33:15 +0200

    fix: duplicate Year closing tag

--- src/App.tsx
+++ src/App.tsx
@@ -112,7 +112,6 @@ function App() {
                 <Month name='September' sum={accumulateMonth(donations['2025'].Sep)} sundays={4} wednesdays={4} pricePerHour={90}/>
                 <Month name='October' sum={accumulateMonth(donations['2025'].Oct)} sundays={4} wednesdays={5} pricePerHour={85}/>
             </Year>
-            </Year>
 
         </Accordion>
       </Flex>
